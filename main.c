114e81677c361d3c32aa41e89a0761235b9c2b66
denny101603 <43618398+denny101603@users.noreply.github.com>
2018-09-26 20:40:42 +0200

    hoooovno

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+ #include <stdio.h>
 
 #include "ERR_CODES.h"
 
@@ -6,6 +6,7 @@ int main(int argc, char **argv)
 {
 	printf("Ahoj, jak se vede, salati? %d :)\n", ERR_OK);
 	//komentar
+	//FUCK YOU!!!
 	getchar();
 	return ERR_OK;
 }
