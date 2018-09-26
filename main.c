eeb6e995cb1aff4134b48872d6dd94d109f5d739
Honza "Berry" Beran <xberan43@stud.fit.vutbr.cz>
2018-09-26 23:16:32 +0200

    Berryho branch

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+/*
+ * TOHLE JE BERRYHO VETEV, VSICHNI VEN!!!
+ */
+
 #include <stdio.h>
 
 int main() {
@@ -6,5 +10,21 @@ int main() {
     //Galantní Jelen
     //Berry was here!
     //z toho by se jeden posral(hlavně denny)
+    int Meduna[4] =[0, 15, 30, 45];
+    int arr_time = 17;
+    int shoda = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        if(arr_time == Meduna[i]) shoda = 1;
+    }
+    if(shoda)
+    {
+        printf("Vstup povolen. \n");
+    }
+    else
+    {
+        printf("A VEN!!!\n");
+    }
+
     return 0;
 }
