89e255877edf5d3dc679c2b629d496c96988ecac
denny101603 <43618398+denny101603@users.noreply.github.com>
2018-10-09 14:11:46 +0200

    denny FSM_states.h

---
+++ fsm_states.h
@@ -0,0 +1,55 @@
+//
+// Created by danbu on 9.10.2018.
+//
+
+#ifndef IFJ_FSM_STATES_H
+#define IFJ_FSM_STATES_H
+enum states {
+    START,
+    LEX_ERROR = 1,
+    IFJ_CODE_PREAM, //JAKOŽE PREAMBULE :)
+    OP_PLUS,
+    OP_MINUS,
+    OP_DIV,
+    OP_MULT,
+    OP_EQAL_0,
+    OP_EQAL_1,
+    OP_EQAL_2,
+    OP_NOT_EQ_0,
+    OP_NOT_EQ_1,
+    OP_MORE_0,
+    OP_MORE_1,
+    OP_MORE_EQUAL,
+    OP_LESS_0,
+    OP_LESS_1,
+    OP_LESS_EQUAL,
+    OP_COMMA,
+    EOL_0,
+    EOL_1,
+    BLOCK_COMMENT_0,
+    BLOCK_COMMENT_1,
+    BLOCK_COMMENT_2,
+    BLOCK_COMMMENT_3,
+    ONE_LINE_COMMENT,
+    ID_0,
+    ID_1,
+    ID_2,
+    KEY_WORD,
+    EOF_STATE,
+    STRING_0,
+    STRING_1,
+    ESCAPE_0,
+    ESCAPE_1,
+    ESCAPE_2,
+    NUMBER_0,
+    INTEGER,
+    FLOAT_0,
+    FLOAT_1,
+    FLOAT_2,
+    FLOAT_EXP_0,
+    FLOAT_EXP_1,
+    FLOAT_EXP_2,
+    ENDSTATE
+};
+
+#endif //IFJ_FSM_STATES_H
--- main.c
+++ main.c
@@ -5,6 +5,7 @@ int main() {
     //ahoj, já jsem carbik a posilam comit
     //Galantní Jelen
     //Berry was here!
+    //TODO
     //z toho by se jeden posral(hlavně denny)
     return 0;
 }
