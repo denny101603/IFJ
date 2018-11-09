515df61fdd884da8cc5a65b38ec0441bd35408c5
Honza "Berry" Beran <xberan43@stud.fit.vutbr.cz>
2018-11-09 15:08:17 +0100

    Berryho branch

--- savo.c
+++ savo.c
@@ -1,4 +1,18 @@
 //
 // Created by janbe on 08.11.2018.
 //
+#include "savo.h"
 
+
+char get_action(Ttoken input_token, Ttoken stack_token)
+{
+    int stack_nonterminal = -1;
+    int input_terminal = -1;
+    //TODO pridelit kazdemu moznemu typu tokenu cislo, odpovidajici radku/sloupci v prec. tabulce
+    //Pr: tokenu typu "-" odpovida radek/sloupec cislo 1 (radek, pokud to je token ze stacku, sloupec, pokud to je token ze vstupu)
+    //TODO pokud
+
+    if (input_terminal == -1 || stack_nonterminal == -1)
+        return '?'; //neni zaznam v tabulce
+    return prec_table[stack_nonterminal][input_terminal];
+}
--- savo.h
+++ savo.h
@@ -6,3 +6,35 @@
 #define IFJ2018_SAVO_H
 
 #endif //IFJ2018_SAVO_H
+#include "fsm.h"
+#include "fsm.c"
+
+char prec_table[15][15] = {//TODO
+       /* + */ {'>','>','<','<','<','>','<','?','>','>','>','>','>','>','>'},
+       /* - */ {'>','>','<','<','<','>','<','?','>','>','>','>','>','>','>'},
+       /* * */ {'>','>','>','>','<','>','<','?','>','>','>','>','>','>','>'},
+       /* / */ {'>','>','>','>','<','>','<','?','>','>','>','>','>','>','>'},
+       /* ( */ {'<','<','<','<','<','=','<','?','<','<','<','<','<','<','<'},
+       /* ) */ {},
+       /* id */{},
+       /* cons */ {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {}
+};
+//todo
+typedef struct{
+
+}TStack;
+//todo
+char get_action(Ttoken input_token, Ttoken stack_token);
+bool is_end_of_expression(Ttoken token);
+
+//todo
+Ttoken get_first_terminal(TStack stack);
+void push(TStack stack, Ttoken token);
+void pop(TStack stack, Ttoken token);
