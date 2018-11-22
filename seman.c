821221646a62f5fb46a4393e1d92f12862c854f1
Honza "Berry" Beran <xberan43@stud.fit.vutbr.cz>
2018-11-22 20:33:42 +0100

    dodelany gen fce

--- seman.c
+++ seman.c
@@ -80,16 +80,16 @@ void TAC_insert_post(TTacList *list, TThreeAC *elem, TThreeAC *inserted)
     if(list == NULL || elem == NULL || inserted == NULL || list->first == NULL)
         return;
 
-    inserted->prev = elem->prev;
+    /* Toto je insert_post, o kterem mi denny rekl, at ho naimplementuju, aby to vzapeti poprel -_- :D
+     inserted->prev = elem->prev;
     if(elem->prev != NULL)
         elem->prev->next = inserted;
     elem->prev = inserted;
     inserted->next = elem;
     if(elem == list->first)
-        list->first = inserted;
+        list->first = inserted;*/
 
-    /*
-        skutecny post, ale my potrebujeme prev :)
+
     inserted->next = elem->next;
     if(elem->next != NULL)
         elem->next->prev = inserted;
@@ -97,7 +97,7 @@ void TAC_insert_post(TTacList *list, TThreeAC *elem, TThreeAC *inserted)
     inserted->prev = elem;
     if(elem == list->last)
          list->last = inserted;
-         */
+
 
 }
 
