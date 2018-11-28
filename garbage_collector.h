c298710a805a19956dbc25c37e0fdbe4f26441d9
Honza "Berry" Beran <xberan43@stud.fit.vutbr.cz>
2018-11-28 18:32:29 +0100

    uppraveno savo a fsm, aby fungovaly floaty

--- garbage_collector.h
+++ garbage_collector.h
@@ -4,5 +4,17 @@
 
 #ifndef IFJ2018_GARBAGE_COLLECTOR_H
 #define IFJ2018_GARBAGE_COLLECTOR_H
+//Funkce a struktury pro garbage collector
+typedef struct gce{
+    void *ptr;
+    struct gce *previous;
+} GC_elem;
+
+typedef struct gc{
+    GC_elem *last_added;
+} Tgarbage_collector;
+
+void gc_add_garbage(void *ptr, Tgarbage_collector *gc);
+void valar_morghulis(Tgarbage_collector *gc);
 
 #endif //IFJ2018_GARBAGE_COLLECTOR_H
